2765edc23b53e84e616d0edb3df5c5d1a57d0966
siddheswarojha <siddheswarojha23@gmail.com>
2023-11-12 01:19:10 +0530

    initial structure

--- app/src/main/java/com/example/png/MainActivity.java
+++ app/src/main/java/com/example/png/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.png;
 
-import androidx.appcompat.app.AppCompatActivity;
+// ... (Your imports and class definition remain the same)
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etMessage;
     Button btnSend;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +34,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String message = etMessage.getText().toString();
-
-
+                sendPostRequest(message);
             }
         });
+    }
 
+    private void sendPostRequest(String message) {
+        String postUrl = "your_post_url";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, postUrl,
+                response -> {
+                    // Handle the successful response
+                }, error -> {
+            // Handle errors
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("message", message);
+                // Add other parameters if required
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
     }
 }
